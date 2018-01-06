f10733fa651141d699622ed450d3ca86ea015a04
Swantje <swantje-t@web.de>
2018-01-06 16:58:37 +0100

    init spring boot

---
+++ src/main/java/com/games/nim13/Nim13Application.java
@@ -0,0 +1,12 @@
+package com.games.nim13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nim13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nim13Application.class, args);
+    }
+}
